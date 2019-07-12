a54227994f605e1b42da696b7e6648cf7ee06971
Roy Chancellor <roychance600@gmail.com>
2019-07-12 09:05:28 -0700

    Updated Roy Branch

--- commonbeginningofwords/src/main/java/cst135/commonbeginningofwords/Controller.java
+++ commonbeginningofwords/src/main/java/cst135/commonbeginningofwords/Controller.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class Controller {
 
 	public static void main(String[] args) throws IOException {
-		
+		//NEW BRANCH ROY
 		// Add more and more comments
 		LongestCommonString lcs = new LongestCommonString();
 		Writer wr = new Writer();
