5196b0348aeda347f684649b48b6e400ec727d5a
RobertLoy <rob.loy@gmail.com>
2019-07-12 09:01:30 -0700

    testing clobbering

--- commonbeginningofwords/src/main/java/cst135/commonbeginningofwords/Controller.java
+++ commonbeginningofwords/src/main/java/cst135/commonbeginningofwords/Controller.java
@@ -25,7 +25,7 @@ public class Controller {
 		for(String word : lcs.getWords())
 			System.out.println("\t" + word);
 		
-		//Find the BIGGEST common beginning string
+		//Find the Smallest common beginning string
 		System.out.println("Biggest common string: " + lcs.longestCommon());
 		
 		//Write the common string to a file
